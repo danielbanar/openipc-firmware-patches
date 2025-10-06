debe6e55d83e6fc35cac8c3d07bd2b12c11a1085
Daniel Banar <daniel.banar5@gmail.com>
2025-10-06 22:59:58 +0200

    gps fix

--- general/package/gps-db/gps-db.mk
+++ general/package/gps-db/gps-db.mk
@@ -14,11 +14,11 @@ endef
 
 define GPS_DB_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/bin $(@D)/gps
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/bin $(@D)/gps_monitor
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/gps
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/gps_monitor
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(@D)/gps.conf
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc $(@D)/gps.conf
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(@D)/S71gps
