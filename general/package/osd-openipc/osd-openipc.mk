29ff9bf35041578b19844e0b012062439fa6bde4
Daniel Banar <daniel.banar5@gmail.com>
2025-10-08 23:09:55 +0200

    added osd for dash config

--- general/package/osd-openipc/osd-openipc.mk
+++ general/package/osd-openipc/osd-openipc.mk
@@ -5,9 +5,14 @@
 ################################################################################
 
 OSD_OPENIPC_SITE = $(call github,danielbanar,openipc-osd,$(OSD_OPENIPC_VERSION))
-OSD_OPENIPC_VERSION = HEAD
 OSD_OPENIPC_LICENSE = MIT
 OSD_OPENIPC_LICENSE_FILES = LICENSE
+
+ifeq ($(OPENIPC_VARIANT),dash)
+	OSD_OPENIPC_VERSION = dash
+else
+	OSD_OPENIPC_VERSION = HEAD
+endif
 
 ifeq ($(BR2_TOOLCHAIN_USES_GLIBC),y)
 	OSD_OPENIPC_OPTIONS = "-rdynamic -s -Os -lm"
@@ -22,9 +27,15 @@ endef
 define OSD_OPENIPC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(@D)/osd.yaml
+	
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/root
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/root $(@D)/openipc_logo.bmp
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 $(@D)/osd $(TARGET_DIR)/usr/bin/osd_server
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(@D)/S97osd
 endef
 
 $(eval $(generic-package))
